71e7c9d04b2ed81369f19cfc28079502d27125f6
scripture <125283797+95348953489345893524897@users.noreply.github.com>
2025-10-22 08:41:49 -0400

    Create convarload.lua

---
+++ GarrysMod/lua/menu/convarload.lua
@@ -0,0 +1,44 @@
+--[[ 
+    This script lets you load convars on joining a server
+    the reason for this is that some convars like cl_detaildist will reset back to their default value on joining a server
+    this cannot be fixed in configs (i think), so we have to use a script to set cl_detaildist to its desired
+
+    https://github.com/RaphaelIT7/gmod-gameevent/releases/ required for the hooks, put the DLL in garrysmod/lua/bin
+    put this script in garrysmod/lua/menu and put this in garrysmod/lua/menu/menu.lua:
+    include("convarload.lua")
+
+]]
+if util.IsBinaryModuleInstalled("gameevent") then
+    require("gameevent")
+else
+    print("no gameevent")
+end
+
+local ConvarTable = {
+    -- put convars you want to set when you load in here
+    ["cl_detaildist"] = "0",
+    ["cl_timeout"] = "100000",
+    --["cl_yourconvar"] = "desiredvalue",
+}
+
+local InitialRun = true
+gameevent.Listen("client_disconnect")
+hook.Add("client_disconnect", "client_disconnect_example", function(data)
+    InitialRun = true -- set InitialRun back to true because we disconnected to join another server
+end)
+
+gameevent.Listen("player_activate") -- "Called when a player has entered the game (connected and loaded)."
+hook.Add("player_activate", "SetConvars", function(data)
+    if InitialRun == true then -- make sure were only running this when loaded into a server for the first time 
+        for convar, argument in pairs(ConvarTable) do
+            RunConsoleCommand(convar, argument)
+            timer.Simple(0.01, function()
+                -- the convar values print correctly if there is no delay
+                print("Setting convar " .. convar .. " to " .. argument)
+                print("Game says " .. convar .. " == " .. GetConVar(convar):GetString()) -- prove we set the convar correctly
+            end)
+        end
+
+        InitialRun = false
+    end
+end)
