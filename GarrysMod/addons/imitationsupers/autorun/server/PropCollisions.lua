8b1646b43d9676d778b72a1592d3d3be969509b3
scripture <125283797+95348953489345893524897@users.noreply.github.com>
2025-10-26 20:21:54 -0400

    Make props change color like supers

--- GarrysMod/addons/imitationsupers/autorun/server/PropCollisions.lua
+++ GarrysMod/addons/imitationsupers/autorun/server/PropCollisions.lua
@@ -1,11 +1,18 @@
+local PickupColor = Color(255, 0, 0, 120)
 hook.Add("OnPhysgunPickup", "NoCollideProp", function(ply, ent)
-    if ent:IsValid() then -- on supers it would become translucent and red(?) on pickup, but im too lazy
+    if ent:IsValid() then
         ent:SetCollisionGroup(1)
+        ent.FormerColor = ent:GetColor()
+        ent.FormerRenderMode = ent:GetRenderMode()
+        ent:SetRenderMode(RENDERMODE_TRANSADD)
+        ent:SetColor(PickupColor)
     end
 end)
 
 hook.Add("PhysgunDrop", "RecollideProp", function(ply, ent)
-    if ent:isValid() then --
+    if ent:IsValid() then
         ent:SetCollisionGroup(0)
+        ent:SetColor(ent.FormerColor)
+        ent:SetRenderMode(ent.FormerRenderMode)
     end
 end)
