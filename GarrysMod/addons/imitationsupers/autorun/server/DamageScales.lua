d46249f8adeb88791faebb70b693bae147989a37
scripture <125283797+95348953489345893524897@users.noreply.github.com>
2025-10-26 20:01:51 -0400

    Finish damagescales

--- GarrysMod/addons/imitationsupers/autorun/server/DamageScales.lua
+++ GarrysMod/addons/imitationsupers/autorun/server/DamageScales.lua
@@ -1,1 +1,14 @@
-write later
+--https://wiki.facepunch.com/gmod/Enums/DMG
+--https://wiki.facepunch.com/gmod/GM:EntityTakeDamage
+-- makes some weapons have no damage like supers (props, fall damage, ar2 orb, rpg) 
+hook.Add("EntityTakeDamage", "EntityDamage", function(target, dmginfo)
+	if target:IsPlayer() then
+		if dmginfo:IsDamageType(32) then -- Fall damage
+			dmginfo:ScaleDamage(0)
+		elseif dmginfo:IsDamageType(1) then -- Prop damage, also ar2 orb damage?
+			dmginfo:ScaleDamage(0)
+		elseif IsValid(dmginfo:GetWeapon()) and dmginfo:GetWeapon():GetClass() == "weapon_rpg" then -- rpg damage
+			dmginfo:ScaleDamage(0)
+		end
+	end
+end)
