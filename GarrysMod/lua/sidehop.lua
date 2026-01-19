f5ad9497d4e427cb7575d337b06822922c9b4adb
scripture <125283797+95348953489345893524897@users.noreply.github.com>
2026-01-19 13:44:04 -0500

    Rewrite sidehop.lua, the old code was very ugly

--- GarrysMod/lua/sidehop.lua
+++ GarrysMod/lua/sidehop.lua
@@ -1,7 +1,17 @@
--- put in garrysmod/lua and do lua_openscript_cl sidehop.lua
-hook.Add("PostPlayerDraw", "DrawArc", function(ply)
-    if not LocalPlayer():ShouldDrawLocalPlayer() then return end
-    if Vector(LocalPlayer():GetVelocity().x, LocalPlayer():GetVelocity().y, 0):Length2D() < 1 then return end
-    render.DrawLine(LocalPlayer():GetShootPos(), LocalPlayer():GetShootPos() + Vector(LocalPlayer():GetVelocity().x, LocalPlayer():GetVelocity().y, 0):GetNormalized() * 50, Color(0, 255, 0), true)
-    render.DrawLine(LocalPlayer():GetShootPos(), LocalPlayer():GetShootPos() + Vector(-Vector(LocalPlayer():GetVelocity().x, LocalPlayer():GetVelocity().y, 0).y, Vector(LocalPlayer():GetVelocity().x, LocalPlayer():GetVelocity().y, 0).x, 0):GetNormalized() * 50, Color(255, 0, 0), true)
+-- The red is your forward velocity
+-- Blue / green = side velocity (what you should be looking at to accelerate)
+--
+-- Rest of script
+local color_blue = Color(0, 0, 255, 255) -- It looks like a gizmo now (not on purpose). I'm not sure how gizmos are actually colored... i just used the simplest colors
+local color_green = Color(0, 255, 0, 255)
+local color_red = Color(255, 0, 0, 255)
+hook.Add("PostPlayerDraw", "Sidehop", function(ply)
+    local vel = Vector(ply:GetVelocity().x, ply:GetVelocity().y, 0)
+    vel:Normalize()
+    local pos = ply:GetPos() + ply:OBBCenter()
+    render.DrawLine(pos, pos + vel * 75, color_red) -- forward 
+    vel:Rotate(Angle(0, 90, 0))
+    render.DrawLine(pos, pos + vel * 75, color_blue) -- the left
+    vel:Rotate(Angle(0, -180, 0))
+    render.DrawLine(pos, pos + vel * 75, color_green) -- the right
 end)
