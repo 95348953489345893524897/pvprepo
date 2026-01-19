ae275c4cf2537056d866ee307fea1806ba680bcb
scripture <125283797+95348953489345893524897@users.noreply.github.com>
2026-01-19 13:56:04 -0500

    Always show lines even while standing still

--- GarrysMod/lua/sidehop.lua
+++ GarrysMod/lua/sidehop.lua
@@ -6,12 +6,15 @@ local color_blue = Color(0, 0, 255, 255) -- It looks like a gizmo now (not on pu
 local color_green = Color(0, 255, 0, 255)
 local color_red = Color(255, 0, 0, 255)
 hook.Add("PostPlayerDraw", "Sidehop", function(ply)
-    local vel = Vector(ply:GetVelocity().x, ply:GetVelocity().y, 0)
-    vel:Normalize()
+    local vel = ply:GetVelocity()
+    local aimvec = ply:GetAimVector()
+    local PlyMoving = vel:Length2DSqr() > 0
+    local visualisation = (PlyMoving and Vector(vel.x, vel.y, 0)) or Vector(aimvec.x, aimvec.y, 0) -- If the player is moving, the visualisation is the velvec, otherwise use the aimvec 
+    visualisation:Normalize()
     local pos = ply:GetPos() + ply:OBBCenter()
-    render.DrawLine(pos, pos + vel * 75, color_red) -- forward 
-    vel:Rotate(Angle(0, 90, 0))
-    render.DrawLine(pos, pos + vel * 75, color_blue) -- the left
-    vel:Rotate(Angle(0, -180, 0))
-    render.DrawLine(pos, pos + vel * 75, color_green) -- the right
+    render.DrawLine(pos, pos + visualisation * 75, color_red) -- forward 
+    visualisation:Rotate(Angle(0, 90, 0))
+    render.DrawLine(pos, pos + visualisation * 75, color_blue) -- the left
+    visualisation:Rotate(Angle(0, -180, 0))
+    render.DrawLine(pos, pos + visualisation * 75, color_green) -- the right]]
 end)
